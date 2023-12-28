5bd2abc72aee16d422922e760b93e08e519bd605
Zainal Arifin <zainal.arifin@mitrais.com>
2023-12-28 20:18:51 +0700

    Update launchSetting. json for clustering

--- src/Study Cases/Clustering/Common/Constants.cs
+++ src/Study Cases/Clustering/Common/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string MODEL_FILENAME = "chapter5.mdl";
+        public const string MODEL_FILENAME = "clustering.mdl";
 
         public const string SAMPLE_DATA = "sampledata.csv";
 
